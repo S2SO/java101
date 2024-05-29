9d62ba9014786dff39327a976fe6a549c632be86
S2SO <133852918+S2SO@users.noreply.github.com>
2024-05-29 20:54:14 +0900

    Update BigToSmall.java

--- src/BigToSmall.java
+++ src/BigToSmall.java
@@ -6,14 +6,13 @@ public class BigToSmall {
         System.out.println("대문자 <-> 소문자 프로그램");
         System.out.println("영단어 입력: ");
         String word = sc.nextLine();
-        int length = word.length();
         String result = "";
 
         // 변수 설정 ver.
         String large = word.toUpperCase();
         String small = word.toLowerCase();
 
-        for (int i=0; i<length; i++){
+        for (int i=0; i<word.length(); i++){
             String ist=word.substring(i,i+1);
             String istL=large.substring(i,i+1);
             String istS=small.substring(i,i+1);
@@ -27,7 +26,7 @@ public class BigToSmall {
         System.out.println(result);
 
         // 변수 미설정 ver.
-//       for (int i=0; i<length; i++){
+//       for (int i=0; i<word.length(); i++){
 //            if (word.substring(i,i+1).equals(word.toUpperCase().substring(i,i+1))) {
 //                result += word.toLowerCase().substring(i,i+1);
 //            }else {
